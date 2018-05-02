e30bc0425aa1b09c4379e8d15506ffe4438a24d7
Josh Mills <josh@trueheart78.com>
2018-05-02 15:13:02 -0400

    :sparkling_heart:

---
+++ taylorTime/taylorTime.go
@@ -0,0 +1,57 @@
+package taylorTime
+
+import (
+	"fmt"
+	"time"
+)
+
+func consume(time, amount int64) (units, seconds int64) {
+	seconds = time
+	for {
+		if seconds < amount {
+			break
+		}
+		seconds -= amount
+		units += 1
+	}
+	return
+}
+
+const day = 86400
+const hour = 3600
+const minute = 60
+
+const longForm = "Jan 2, 2006 at 3:04pm (MST)"
+
+var loc, _ = time.LoadLocation("UTC")
+var concertTime, _ = time.ParseInLocation(longForm, "Jul 7, 2018 at 7:00pm (EDT)", loc)
+
+func timeToDate(sec int64) (days, hours, minutes, seconds int64) {
+	seconds = sec
+	days, seconds = consume(seconds, day)
+	hours, seconds = consume(seconds, hour)
+	minutes, seconds = consume(seconds, minute)
+	return
+}
+
+func Future() bool {
+	return time.Now().Before(concertTime)
+}
+
+func Happened() bool {
+	return !Future()
+}
+
+func Banner() string {
+	return fmt.Sprintf("Taylor Swift is in Columbus, Ohio on %v, %v %vth, %v at 7pm", concertTime.Weekday(), concertTime.Month(), concertTime.Day(), concertTime.Year())
+}
+
+func TimeRemaining() string {
+	if Future() {
+		x := time.Until(concertTime)
+		d, h, m, s := timeToDate(int64(x.Seconds()))
+		return fmt.Sprintf("[ %d days, %d hours, %d minutes, %d seconds ]", d, h, m, s)
+	} else {
+		return fmt.Sprint("The concert was awesome!")
+	}
+}
--- timeToTaylor.go
+++ timeToTaylor.go
@@ -2,46 +2,11 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"github.com/trueheart78/timeToTaylor/taylorTime"
 )
 
-func consume(time, amount int64) (units, seconds int64) {
-	seconds = time
-	for {
-		if seconds < amount {
-			break
-		}
-		seconds -= amount
-		units += 1
-	}
-	return
-}
-
-const day = 86400
-const hour = 3600
-const minute = 60
-
-func timeToDate(sec int64) (days, hours, minutes, seconds int64) {
-	seconds = sec
-	days, seconds = consume(seconds, day)
-	hours, seconds = consume(seconds, hour)
-	minutes, seconds = consume(seconds, minute)
-	return
-}
-
 func main() {
-	loc, _ := time.LoadLocation("UTC")
-
-	const longForm = "Jan 2, 2006 at 3:04pm (MST)"
-	concertTime, _ := time.ParseInLocation(longForm, "Jul 7, 2018 at 7:00pm (EDT)", loc)
-
-	fmt.Printf("Taylor Swift is in Columbus, Ohio on %v, %v %vth, %v at 7pm\n\n", concertTime.Weekday(), concertTime.Month(), concertTime.Day(), concertTime.Year())
-
-	if time.Now().Before(concertTime) {
-		x := time.Until(concertTime)
-		d, h, m, s := timeToDate(int64(x.Seconds()))
-		fmt.Printf("[ %d days, %d hours, %d minutes, %d seconds ]\n", d, h, m, s)
-	} else {
-		fmt.Println("The concert was awesome!\n")
-	}
+	fmt.Println(taylorTime.Banner())
+	fmt.Println("")
+	fmt.Println(taylorTime.TimeRemaining())
 }
